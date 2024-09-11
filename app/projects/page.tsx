40e370ac9cddfa51032df03e5d77c370818d5fe0
mohitsharma012 <stayer.mohit@gmail.com>
2024-09-11 16:03:17 +0530

    error fix

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import projectDatabase from "@/lib/projectDatabase";
+import projectDatabase from "../../lib/projectDatabase";
 
 const page = () => {
   return (
