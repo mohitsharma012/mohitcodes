bdcacc67449401ab4b6130a49b80a2dfa6317c60
mohitsharma012 <stayer.mohit@gmail.com>
2025-02-11 17:39:52 +0530

    default theme

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar = () => {
   const [theme, setTheme] = useState("dark");
 
   useEffect(() => {
-    const savedTheme = localStorage.getItem("theme") || "light";
+    const savedTheme = localStorage.getItem("theme") || "dark";
     setTheme(savedTheme);
     document.documentElement.className = savedTheme; 
   }, []);
